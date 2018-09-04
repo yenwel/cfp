f4af5d6e3bd5d16e90235c6d6e1d025e9aea4ee5
Jan Vandepitte <Jan.Vandepitte@mediahuis.be>
2018-09-04 10:53:11 +0200

    test rust

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,8 @@
-fn identity<T> (me:T) -> T {
+pub fn identity<T> (me:T) -> T {
     me
 }
 
-fn compose<Fa:'static ,Fb: 'static,Ta,Tb,Tc> (fa : Fa, fb : Fb) -> Box<Fn(Ta) -> Tc>
+pub fn compose<Fa:'static ,Fb: 'static,Ta,Tb,Tc> (fa : Fa, fb : Fb) -> Box<Fn(Ta) -> Tc>
 where Fa: Fn(Ta) -> Tb, Fb : Fn(Tb) -> Tc
 {
    return Box::new(move | a : Ta | fb(fa(a)));
@@ -10,9 +10,14 @@ where Fa: Fn(Ta) -> Tb, Fb : Fn(Tb) -> Tc
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Neg;
+
     #[test]
     fn composition_preserves_identity() 
     {
-        assert!(1==1)
+        let f_one = super::compose(super::identity, i8::neg);
+        let f_two = super::compose(i8::neg, super::identity);
+        assert!(f_one(8)==f_two(8));
+        assert!(f_one(-8)==f_two(-8));        
     }
 }
