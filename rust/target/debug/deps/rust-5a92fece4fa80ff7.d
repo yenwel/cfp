62ae5453dc563e98a2f92310fa9f9e8aaffbcbcc
Jan Vandepitte <Jan.Vandepitte@mediahuis.be>
2018-09-05 13:13:52 +0200

    clean up

--- rust/target/debug/deps/rust-5a92fece4fa80ff7.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jnve11\source\cfp-csharp-and-rust\rust\target\debug\deps\librust-5a92fece4fa80ff7.rlib: src\lib.rs
-
-C:\Users\jnve11\source\cfp-csharp-and-rust\rust\target\debug\deps\rust-5a92fece4fa80ff7.d: src\lib.rs
-
-src\lib.rs:
--- rust/target/debug/librust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\jnve11\source\cfp-csharp-and-rust\rust\target\debug\librust.rlib: C:\Users\jnve11\source\cfp-csharp-and-rust\rust\src\lib.rs
