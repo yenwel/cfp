1a8d1a1ad61602284c243d0a6e0afb817856db06
Jan Vandepitte <Jan.Vandepitte@mediahuis.be>
2018-09-11 13:18:04 +0200

    test memoize dotnet

--- dotnet-test/CategoryTheoryForProgrammersTests.cs
+++ dotnet-test/CategoryTheoryForProgrammersTests.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using dotnet;
+using System.Threading;
 
 namespace dotnet_test
 {
@@ -9,10 +10,25 @@ namespace dotnet_test
         [Fact]
         public void CompositionPreservesIdentity()
         {            
-            var f_one = CategoryTheoryForProgrammers.Compose<int,int,int>(CategoryTheoryForProgrammers.Identity,(int a) => -a);
-            var f_two = CategoryTheoryForProgrammers.Compose<int,int,int>((int a) => -a, CategoryTheoryForProgrammers.Identity);
-            Assert.True(f_one(8)==f_two(8));
-            Assert.True(f_one(-8)==f_two(-8));
+            var one = CategoryTheoryForProgrammers.Compose<int,int,int>(CategoryTheoryForProgrammers.Identity,(int a) => -a);
+            var two = CategoryTheoryForProgrammers.Compose<int,int,int>((int a) => -a, CategoryTheoryForProgrammers.Identity);
+            Assert.True(one(8)==two(8));
+            Assert.True(one(-8)==two(-8));
+        }
+
+         [Fact]
+        public void MemoizeWorks()
+        {            
+            Func<int,int> addOneLongRunning = (int a) => { Thread.Sleep(50); return a++; };
+            var addOneLongRunningMemoized = CategoryTheoryForProgrammers.Memoize(addOneLongRunning);
+            var time = DateTime.Now;        
+            Console.WriteLine(time);  
+            Assert.True(addOneLongRunning(42)==addOneLongRunning(42));
+            var runtimeOne = ( DateTime.Now - time);
+            time = DateTime.Now;
+            Assert.True(addOneLongRunning(42)==addOneLongRunning(42)); 
+            var runtimeTwo = (DateTime.Now - time );
+            Assert.True(runtimeTwo < runtimeOne );
         }
     }
 }
